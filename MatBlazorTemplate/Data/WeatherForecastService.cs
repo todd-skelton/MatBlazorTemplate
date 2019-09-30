3ce2fd599d45aac014cbd954a33ff7d6891c163f
Todd Skelton <todd.skelton@outlook.com>
2019-09-29 20:55:30 -0500

    Add artificial delay to show loading bar.

--- MatBlazorTemplate/Data/WeatherForecastService.cs
+++ MatBlazorTemplate/Data/WeatherForecastService.cs
@@ -11,15 +11,17 @@ namespace MatBlazorTemplate.Data
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+        public async Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
         {
+            await Task.Delay(1000);
+
             var rng = new Random();
-            return Task.FromResult(Enumerable.Range(1, 100).Select(index => new WeatherForecast
+            return Enumerable.Range(1, 100).Select(index => new WeatherForecast
             {
                 Date = startDate.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
-            }).ToArray());
+            }).ToArray();
         }
     }
 }
